ea54b23ab60f8401c19aafe332ac5d1f19bb380d
Auriga <area51alienware@gmail.com>
2016-01-11 18:33:31 -0800

    Fixed the background repeat + Added level up pictures.

--- characters/character-js/abaddon.js
+++ characters/character-js/abaddon.js
@@ -1,5 +1,15 @@
+var q = 0;
+var w = 0;
+var e = 0;
+var r = 0;
+var maxq = 4;
+var maxw = 4;
+var maxe = 4;
+var maxr = 3;
 
 $(document).ready(function()
 {
-
+  $("#levelq").click(function(){
+      $("p").hide();
+  });  
 });
